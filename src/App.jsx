f148005a914a5f9313596387e1c78ad617de86ff
chanchal3103 <chanchalsaini3103@gmail.com>
2025-11-21 11:51:30 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
-import ServicesSection from './components/ServicesSection';
 import PricingSection from './components/PricingSection';
-import PortfolioSection from './components/PortfolioSection';
-import TeamSection from './components/TeamSection';
-import FAQSection from './components/FAQ';
-import ContactSection from './components/ContactSection';
-import ClientReviews from './components/ClientReviews';
 import Testimonials from './components/Testimonials';
-import FAQ from './components/FAQ';
 import SupportSection from './components/SupportSection';
-import WorkflowTimeline from './components/WorkflowZigZag';
+import WorkflowZigZag from './components/WorkflowZigZag';
 import "aos/dist/aos.css";
 import AOS from "aos";
 AOS.init();
@@ -22,16 +15,10 @@ function App() {
       <Navbar />
       <HeroSection />
       <AboutSection />
-      <WorkflowTimeline />
+      <WorkflowZigZag />
       <PricingSection />
-      
-   
-     
-     
-      
       <Testimonials />
       <SupportSection />
-      {/* maybe a footer too */}
     </>
   );
 }
